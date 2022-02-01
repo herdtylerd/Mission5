e77c3848d04c969b15d20162a3f96aeca616e7d1
herdtylerd <91562573+herdtylerd@users.noreply.github.com>
2022-02-01 11:12:47 -0700

    Initial commit

---
+++ Mission4/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Mission5.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission5.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        private MovieContext MovieContext { get; set; }
+
+        public HomeController(ILogger<HomeController> logger, MovieContext someName)
+        {
+            _logger = logger;
+            MovieContext = someName;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+            
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Podcasts()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Movies()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Movies(Movie m)
+        {
+            MovieContext.Add(m);
+            MovieContext.SaveChanges();
+            return View("Index");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Mission4/Models/MovieContext.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission5.Models
+{
+    public class MovieContext : DbContext
+    {
+        public MovieContext (DbContextOptions<MovieContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+
+        // Seed database
+        protected override void OnModelCreating(ModelBuilder mb)
+        {
+            mb.Entity<Movie>().HasData(
+                new Movie
+                {
+                    MovieId = 1,
+                    Category = "Comedy/Drama",
+                    Title = "The Intern",
+                    Year = 2015,
+                    Director = "Nancy Meyers",
+                    Rating = "PG13"
+                },
+
+                new Movie
+                {
+                    MovieId = 2,
+                    Category = "Fantasy",
+                    Title = "Harry Potter and the Deathly Hallows: Part 2",
+                    Year = 2011,
+                    Director = "David Yates",
+                    Rating = "PG13"
+                },
+
+                new Movie
+                {
+                    MovieId = 3,
+                    Category = "Family/Comedy",
+                    Title = "The Santa Clause",
+                    Year = 1994,
+                    Director = "John Pasquin",
+                    Rating = "PG"
+                }
+
+            );
+        }
+    }
+}
